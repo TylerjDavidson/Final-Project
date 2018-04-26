4407867405df7c05b7d9d3b6e1b1f85264f5ae03
tylerjdavidson <tyler.jg.davidson@gmail.com>
2018-04-26 11:32:00 -0400

    kill

--- server.py
+++ server.py
@@ -19,11 +19,8 @@ def data():
     s.write("p") #send cmd to send serial data from Arduino
     str_data = s.readline() #read data
     data = [int(x) for x in str_data.split(',')] #split data into two variables
-    # TODO read temperature and humidity from Arduino
-    #convert to Fahrenheit
     indoor_temp = round(data[0]*1.8+32)
     indoor_humidity = data[1]
-    # TODO read temperature and humidity from openweathermap.org
     payload = {'q': 'Annapolis','units':'imperial','APPID':'631ff851f1ece3754797d45cd9573bb0'}
     r = requests.get('http://api.openweathermap.org/data/2.5/weather', params=payload)
     r.json()
@@ -41,15 +38,9 @@ def data():
 def cheep():
     name = request.form['name']
     message = request.form['message']
-    #print("got a cheep from [%s]: %s" % (name,message))
-    with open("cheeps.log",'a') as f:
-        f.write("%s: %s" %(name,message))
-    # TODO: append [name: message] to a file of cheeps
     with open("cheeps.log",'a') as f:
         f.write("%s: %s" % (name,message))
-    # TODO: display the cheep on the kit LCD
-
-    s.write("l")
+    s.write('l')
     msg = "%s: %s" % (name,message)
     s.write(msg.encode('utf-8'))
     return render_template('thankyou.html')
